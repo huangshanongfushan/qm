dc88d96ff0359c5b034903d043644b1db57d0c3a
mli <meng.li@duplicall.com>
2017-02-16 15:31:13 +0800

    各模块pom.xml 更新，删除Controller和service 中相关无用的引用

--- app/src/main/java/com/duplicall/qm/app/controller/UserController.java
+++ app/src/main/java/com/duplicall/qm/app/controller/UserController.java
@@ -1,16 +1,13 @@
 package com.duplicall.qm.app.controller;
 
 import com.duplicall.qm.biz.service.user.IUser;
-import com.duplicall.qm.common.model.User;
 import com.duplicall.qm.common.model.Users;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Administrator on 2017/2/7.
@@ -27,29 +24,5 @@ public class UserController {
         return userImpl.queryById(id);
     }
 
-    /**
-     * Post 请求
-     *
-     * @param request
-     * @param user
-     * @return
-     */
-    @RequestMapping(value = "user", method = RequestMethod.POST)
-    public String addUser(HttpServletRequest request, @RequestBody User[] user) {
-        logger.info("post method");
-        return "";
-    }
 
-    @RequestMapping("users/info")
-    public List<User> queryAllUsers() {
-        logger.info("query all users ");
-        User user = new User();
-        user.setLoginId("admin");
-        User user1 = new User();
-        user1.setLoginId("admin1");
-        List<User> userList = new ArrayList<>();
-        userList.add(user);
-        userList.add(user1);
-        return userList;
-    }
 }
--- biz/src/main/java/com/duplicall/qm/biz/service/user/IUser.java
+++ biz/src/main/java/com/duplicall/qm/biz/service/user/IUser.java
@@ -1,13 +1,11 @@
 package com.duplicall.qm.biz.service.user;
 
-import com.duplicall.qm.common.model.User;
 import com.duplicall.qm.common.model.Users;
 
 /**
  * Created by Administrator on 2017/2/7.
  */
 public interface IUser {
-    void addUser(User user);
 
     Users queryById(int id);
 }
