648987d347504e6726858aebd2d04a6e4b77e134
mli <meng.li@duplicall.com>
2017-02-17 15:56:55 +0800

    User service 增加查询功能

--- biz/src/main/java/com/duplicall/qm/biz/service/user/IUser.java
+++ biz/src/main/java/com/duplicall/qm/biz/service/user/IUser.java
@@ -1,6 +1,9 @@
 package com.duplicall.qm.biz.service.user;
 
 import com.duplicall.qm.common.model.Users;
+import com.duplicall.qm.common.model.UsersExample;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/2/7.
@@ -8,4 +11,5 @@ import com.duplicall.qm.common.model.Users;
 public interface IUser {
 
     Users queryById(int id);
+    List<Users> selectByExample(UsersExample usersExample);
 }
